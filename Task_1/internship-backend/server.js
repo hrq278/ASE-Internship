8964bed47203037e3f49274a3d717de06401f6e9
Haroon Rasheed <hrq278@gmail.com>
2026-07-01 15:32:50 +0500

    Backend Task Done

---
+++ Task_1/internship-backend/server.js
@@ -0,0 +1,48 @@
+import express from "express";
+
+const app = express();  
+const PORT = 5000;
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+} );
+
+
+
+//Routes 
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Allied Software Engineers Internship Program");
+});
+
+
+app.get("/api/company", (req, res) => {
+    const companyInfo = {
+        name: "Allied Software Engineers",
+        location: "Pakistan"
+    }
+    res.json(companyInfo);
+})
+
+app.get("/api/interns" , (req, res) => {
+    const interns = {
+        name: "Haroon Rasheed",
+        domain: "MERN Stack Web Development",
+        batch: "Batch 1",
+        duration: "2 Months",
+        supervisor: "Muhammad Ahsan Ali"
+    }
+
+    res.json(interns);
+})
+
+app.get("/api/tasks", (req, res) => {
+    const tasks = [
+        {taskId: 1, taskName: "Task 1", status: "In Progress"},
+        {taskId: 2, taskName: "Task 2", status: "Not Started"},
+        {taskId: 3, taskName: "Task 3", status: "Not Started"},
+    ]
+    res.json(tasks);
+});
+
